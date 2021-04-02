ff2755de69aa1322a970616aedee3d2b512b6c88
Alexei <AlexeiArtyomov0206@gmail.com>
2021-04-02 05:16:37 +0300

    Add Conversion

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,13 +28,278 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.labelSize = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.buttonDraw = new System.Windows.Forms.Button();
+            this.buttonConvert = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.NUDY = new System.Windows.Forms.NumericUpDown();
+            this.NUDX = new System.Windows.Forms.NumericUpDown();
+            this.NUDSize = new System.Windows.Forms.NumericUpDown();
+            this.label8 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.NUDY)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.NUDX)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.NUDSize)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(15, 15);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(6);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(600, 600);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.labelSize);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(627, 15);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(6);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(6);
+            this.groupBox1.Size = new System.Drawing.Size(329, 177);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Setting";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(173, 126);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(30, 24);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "30";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(173, 86);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(30, 24);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "90";
+            // 
+            // labelSize
+            // 
+            this.labelSize.AutoSize = true;
+            this.labelSize.Location = new System.Drawing.Point(173, 46);
+            this.labelSize.Name = "labelSize";
+            this.labelSize.Size = new System.Drawing.Size(87, 24);
+            this.labelSize.TabIndex = 1;
+            this.labelSize.Text = "600 * 600";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(25, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(123, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Platform Size:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(25, 86);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(137, 24);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Platform Angle:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(25, 126);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(105, 24);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Arm Angle:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(24, 98);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(29, 24);
+            this.label6.TabIndex = 2;
+            this.label6.Text = "X:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(24, 144);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(27, 24);
+            this.label7.TabIndex = 4;
+            this.label7.Text = "Y:";
+            // 
+            // buttonDraw
+            // 
+            this.buttonDraw.Location = new System.Drawing.Point(174, 95);
+            this.buttonDraw.Name = "buttonDraw";
+            this.buttonDraw.Size = new System.Drawing.Size(100, 33);
+            this.buttonDraw.TabIndex = 6;
+            this.buttonDraw.Text = "Draw";
+            this.buttonDraw.UseVisualStyleBackColor = true;
+            this.buttonDraw.Click += new System.EventHandler(this.buttonDraw_Click);
+            // 
+            // buttonConvert
+            // 
+            this.buttonConvert.Location = new System.Drawing.Point(174, 141);
+            this.buttonConvert.Name = "buttonConvert";
+            this.buttonConvert.Size = new System.Drawing.Size(100, 33);
+            this.buttonConvert.TabIndex = 6;
+            this.buttonConvert.Text = "Convert";
+            this.buttonConvert.UseVisualStyleBackColor = true;
+            this.buttonConvert.Click += new System.EventHandler(this.buttonConvert_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.NUDY);
+            this.groupBox2.Controls.Add(this.NUDX);
+            this.groupBox2.Controls.Add(this.NUDSize);
+            this.groupBox2.Controls.Add(this.label8);
+            this.groupBox2.Controls.Add(this.buttonConvert);
+            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Controls.Add(this.buttonDraw);
+            this.groupBox2.Controls.Add(this.label7);
+            this.groupBox2.Location = new System.Drawing.Point(630, 210);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(326, 199);
+            this.groupBox2.TabIndex = 7;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Canvas";
+            // 
+            // NUDY
+            // 
+            this.NUDY.Location = new System.Drawing.Point(59, 142);
+            this.NUDY.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.NUDY.Name = "NUDY";
+            this.NUDY.Size = new System.Drawing.Size(100, 29);
+            this.NUDY.TabIndex = 7;
+            this.NUDY.Value = new decimal(new int[] {
+            400,
+            0,
+            0,
+            0});
+            // 
+            // NUDX
+            // 
+            this.NUDX.Location = new System.Drawing.Point(59, 96);
+            this.NUDX.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.NUDX.Name = "NUDX";
+            this.NUDX.Size = new System.Drawing.Size(100, 29);
+            this.NUDX.TabIndex = 7;
+            this.NUDX.Value = new decimal(new int[] {
+            400,
+            0,
+            0,
+            0});
+            // 
+            // NUDSize
+            // 
+            this.NUDSize.Location = new System.Drawing.Point(174, 46);
+            this.NUDSize.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.NUDSize.Minimum = new decimal(new int[] {
+            100,
+            0,
+            0,
+            0});
+            this.NUDSize.Name = "NUDSize";
+            this.NUDSize.Size = new System.Drawing.Size(100, 29);
+            this.NUDSize.TabIndex = 7;
+            this.NUDSize.Value = new decimal(new int[] {
+            600,
+            0,
+            0,
+            0});
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(24, 46);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(123, 24);
+            this.label8.TabIndex = 2;
+            this.label8.Text = "Platform Size:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(976, 631);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(6);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.NUDY)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.NUDX)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.NUDSize)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label labelSize;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button buttonDraw;
+        private System.Windows.Forms.Button buttonConvert;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.NumericUpDown NUDY;
+        private System.Windows.Forms.NumericUpDown NUDX;
+        private System.Windows.Forms.NumericUpDown NUDSize;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -12,9 +12,139 @@ namespace PolarCoordinates
 {
     public partial class Form1 : Form
     {
+        private float sizePlatform = 600;
+        private Pen penPlatformTemplate = new Pen(Color.Green, 1);
+        private Pen penPlatformFore = new Pen(Color.Green, 3);
+        private Pen penPlatformLine = new Pen(Color.Green, 2);
+        private Pen penArmTemplate = new Pen(Color.Blue, 1);
+        private Pen penArmFore = new Pen(Color.Blue, 3);
+        private Pen penArmLine = new Pen(Color.Blue, 2);
+        private Pen penMainTemplate = new Pen(Color.Red, 1);
+        private Pen penMainFore = new Pen(Color.Red, 3);
+        private Pen penMainLine = new Pen(Color.Red, 2);
+        private Point pointOrg = new Point(-100, -100);
+        private List<PolarPoint> polarPoints = new List<PolarPoint>();
         public Form1()
         {
             InitializeComponent();
         }
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics graphics = e.Graphics;
+            graphics.ScaleTransform(1 / sizePlatform * pictureBox1.Width, -1 / sizePlatform * pictureBox1.Height);
+            graphics.TranslateTransform(0, -1 * sizePlatform);
+            drawTemplates(graphics);
+            if(pointOrg.X > 0 && pointOrg.Y > 0)
+            {
+                drawOrgPoint(graphics);
+            }
+            if(polarPoints.Count > 0)
+            {
+                darwPolarPoints(graphics);
+            }
+        }
+        private void darwPolarPoints(Graphics graphics)
+        {
+            float r1 = sizePlatform / (float)Math.Sqrt(2);
+            for (int i = 0; i < polarPoints.Count; i ++)
+            {
+                double piAngle = polarPoints[i].AngleArm / 180 * Math.PI;
+                float x = (float)Math.Cos(piAngle) * r1;
+                float y = (float)Math.Sin(piAngle) * r1;
+                graphics.DrawEllipse(penMainFore, x - 5, y - 5, 10, 10);
+                graphics.DrawString((i + 1).ToString(), new Font(FontFamily.GenericSansSerif, 12), new SolidBrush(Color.Black), x, y);
+                graphics.DrawLine(penPlatformLine, sizePlatform / 2, sizePlatform / 2, x, y);
+                graphics.DrawLine(penArmLine, 0, 0, x, y);
+            }
+        }
+        private void drawOrgPoint(Graphics graphics)
+        {
+            float r2 = (float)Math.Sqrt(Math.Pow(pointOrg.X - sizePlatform / 2, 2) + Math.Pow(pointOrg.Y - sizePlatform / 2, 2));
+            graphics.DrawEllipse(penMainTemplate, sizePlatform / 2 - r2, sizePlatform / 2 - r2, r2 * 2, r2 * 2);
+            graphics.DrawEllipse(penMainFore, pointOrg.X - 5, pointOrg.Y - 5, 10, 10);
+        }
+        private void drawTemplates(Graphics graphics)
+        {
+            graphics.DrawLine(penPlatformTemplate, 0, sizePlatform / 2, sizePlatform, sizePlatform / 2);
+            graphics.DrawLine(penPlatformTemplate, sizePlatform / 2, 0, sizePlatform / 2, sizePlatform);
+            graphics.DrawEllipse(penPlatformTemplate, 0, 0, sizePlatform, sizePlatform);
+            graphics.DrawEllipse(penPlatformFore, sizePlatform / 2 - 5, sizePlatform / 2 - 5, 10, 10);
+
+            float r1 = sizePlatform / (float)Math.Sqrt(2);
+            graphics.DrawEllipse(penArmTemplate, -r1, -r1, r1 * 2, r1 * 2);
+            graphics.DrawEllipse(penArmFore, -5, -5, 10, 10);
+        }
+        private void buttonDraw_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                sizePlatform = (float)NUDSize.Value;
+                labelSize.Text = string.Format("{0} * {0}", sizePlatform);
+                pointOrg.X = (int)NUDX.Value;
+                pointOrg.Y = (int)NUDY.Value;
+                if (pointOrg.X > sizePlatform || pointOrg.Y > sizePlatform)
+                    return;
+                pictureBox1.Invalidate();
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+        private void buttonConvert_Click(object sender, EventArgs e)
+        {
+            buttonDraw_Click(sender, e);
+            Conversion conversion = new Conversion(sizePlatform / 2);
+            polarPoints = conversion.CatisionToPolar(pointOrg);
+            pictureBox1.Invalidate();
+        }
+    }
+    public class Conversion
+    {
+        private double radiusPlatform = 400;
+        private double radiusArm;
+        private double sqrt2 = Math.Sqrt(2);
+        public Conversion(double rPlat = 400)
+        {
+            radiusPlatform = rPlat;
+            radiusArm = sqrt2 * rPlat;
+        }
+        public List<PolarPoint> CatisionToPolar(Point pos)
+        {
+            double r1 = radiusArm;
+            double r2 = Math.Sqrt(Math.Pow(pos.X - radiusPlatform, 2) + Math.Pow(pos.Y - radiusPlatform, 2));
+            if (r2 > r1 * 2)
+            {
+                Console.WriteLine("Platform Radius must be lower than double of Arm Radius");
+                return null;
+            }
+            double[] sinPlatAngle = new double[2];
+            double sqrtUP = Math.Sqrt(2 - Math.Pow(r2 / r1, 2) / 2);
+            sinPlatAngle[0] = (-1 / sqrt2 * r2 / r1 + sqrtUP) / 2;
+            sinPlatAngle[1] = (-1 / sqrt2 * r2 / r1 - sqrtUP) / 2;
+            //sinPlatAngle[2] = (1 / sqrt2 * r2 / r1 + sqrtUP) / 2;
+            //sinPlatAngle[3] = (1 / sqrt2 * r2 / r1 - sqrtUP) / 2;
+            List<PolarPoint> listAngles = new List<PolarPoint>();
+            foreach(double sinP in sinPlatAngle)
+            {
+                if (Math.Abs(sinP) > 1) continue;
+                double sinA = sinP * r2 / r1 + 1 / sqrt2;
+                if (Math.Abs(sinA) > 1) continue;
+                PolarPoint polarPoint;
+                polarPoint.AngleArm = Math.Asin(sinA) * 180 / Math.PI;
+                polarPoint.AnglePlatform = Math.Asin(sinP) * 180 / Math.PI;
+                if (polarPoint.AnglePlatform > 0)
+                    polarPoint.AnglePlatform = 180 - polarPoint.AnglePlatform;
+                Console.WriteLine("AngleArm = {0}, AnglePlatform = {1}", polarPoint.AngleArm, polarPoint.AnglePlatform);
+                listAngles.Add(polarPoint);
+            }
+            Console.WriteLine("--------------------------------------------------");
+            return listAngles;
+        }
+    }
+    public struct PolarPoint
+    {
+        public double AngleArm;
+        public double AnglePlatform;
     }
 }
